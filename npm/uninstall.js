b7da07c32684ec8f96653b99b817add7ae5a8332
Arnel Helja <ahelja@alpenite.com>
2021-05-02 09:13:28 +0200

    Update uninstall.js

--- npm/uninstall.js
+++ npm/uninstall.js
@@ -1,15 +1,2 @@
-function getBinary() {
-    try {
-        const getBinary = require('./getBinary');
-        return getBinary();
-    } catch (err) { }
-}
-
-const binary = getBinary();
-if (binary) {
-    binary.uninstall();
-}
-
-
 const getBinary = require('./getBinary');
 getBinary().uninstall();
